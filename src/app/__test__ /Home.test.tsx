3efe31daa0123b8082508b6033d9af41d3c44555
andrew <andrewkalumba29@gmail.com>
2025-08-18 10:53:25 +0200

    added Home test

---
+++ src/app/__test__ /Home.test.tsx
@@ -0,0 +1,8 @@
+import SpanishFood from "@/components/SpanishFood"
+import {render, screen} from "@testing-library/react"
+
+test("Has Spanish Food", () => {
+    render(<SpanishFood/>)
+    const foodData = screen.getByTestId("spanishInfo")
+    expect(foodData).toBeInTheDocument()
+})
