63ab552be9798f4fc665646d9d96b69a74177c72
May Sunktong <pattaranan.sunktong@gmail.com>
2025-08-19 23:53:04 +0200

    done test on footer

--- src/__tests__ /Footer.test.tsx
+++ src/__tests__ /Footer.test.tsx
@@ -1,9 +1,9 @@
-import {render, screen} from "@testing-library/react"
-import Footer from "."
+import { render, screen } from "@testing-library/react";
+import Footer from "../components/Footer";
 
 test("if it has a footer", () => {
-    render(<Footer/>)
+  render(<Footer />);
 
-    const footerInfo = screen.getByText(/travel Blog/i)
-    expect(footerInfo).toBeInTheDocument
-})
+  const footerInfo = screen.getByText(/travel Blog/i);
+  expect(footerInfo).toBeInTheDocument;
+});
