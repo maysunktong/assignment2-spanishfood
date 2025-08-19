6d8b356c59a7381b5c5d4e85c6c12af757dfdcc9
May Sunktong <pattaranan.sunktong@gmail.com>
2025-08-20 01:10:11 +0200

    Delete import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Footer from "../components/Footer";
-import Header from "../components/Header";
 
 export const metadata: Metadata = {
   title: "Spanish Food",
